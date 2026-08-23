0c76e5d1a6de3a3c2d839c0c6d9fabaf1e02ed81
Luis Eduardo <eduardo.devop@gmail.com>
2026-08-22 23:24:39 -0600

    feat(build): add DNT build script, npm task, and lock dependencies

---
+++ build.ts
@@ -0,0 +1,44 @@
+import { build, emptyDir, LibName } from "@deno/dnt";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./src/main.ts"],
+  outDir: "./npm",
+  shims: {
+    deno: "dev",
+  },
+  compilerOptions: {
+    lib: ["ES2025", "DOM"] as LibName[],
+    target: "ES2015",
+  },
+  package: {
+    name: "@varavel/nodx",
+    description: "NodX implementation for TypeScript",
+    version: Deno.args[0],
+    license: "MIT",
+    author: "Varavel",
+    homepage: "https://github.com/varavelio/nodxts",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/varavelio/nodxts.git",
+    },
+    bugs: {
+      url: "https://github.com/varavelio/nodxts/issues",
+    },
+    keywords: [
+      "typescript",
+      "nodx",
+      "html",
+      "template-engine",
+      "varavel",
+      "deno",
+      "node",
+      "nodejs",
+    ],
+  },
+  postBuild() {
+    Deno.copyFileSync("LICENSE", "npm/LICENSE");
+    Deno.copyFileSync("README.md", "npm/README.md");
+  },
+});
