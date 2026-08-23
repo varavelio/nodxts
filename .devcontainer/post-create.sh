aa5021a3d126bd79bafd206e98e3c3d128dbe394
Luis Eduardo <eduardo.devop@gmail.com>
2026-08-22 23:03:43 -0600

    feat(devcontainer): add Dockerfile, devcontainer.json, and post-create script

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Create the root's .bashrc
+cat << 'EOF' >> /root/.bashrc
+
+# Define command aliases
+alias ll='ls -alh'
+alias c='clear'
+
+# Set the user file-creation mode mask to 000
+umask 000
+
+EOF
+
+# Configure file permissions for the /workspaces directory
+chmod -R 777 /workspaces
+
+# Configure git
+git config core.fileMode false
